8089b620e060e5a4baa44fb5331d8cb7d53ba01f
jounikoo <jouni.kullervo.kari@gmail.com>
2020-07-14 09:35:21 +0300

    first production build of notes app

---
+++ build/precache-manifest.ea0eaf9d167639e7f130fdabf959db33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a8af9f46959c966d7b9a714abbb6e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bed8d59748dcb25ef3f",
+    "url": "/static/css/main.24e34c59.chunk.css"
+  },
+  {
+    "revision": "b2a0c08bcaf7b20add9a",
+    "url": "/static/js/2.3cff3dcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3cff3dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bed8d59748dcb25ef3f",
+    "url": "/static/js/main.a6676d8b.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
