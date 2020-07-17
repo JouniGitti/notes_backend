7475d0087fe2c1ea4fb2f034211df518d587c014
jounikoo <jouni.kullervo.kari@gmail.com>
2020-07-17 18:32:28 +0300

    Added functionalities, connect to MongoDB and Heroku

---
+++ build/precache-manifest.67d36883ff0a011f7e60604a8e7e84d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9216bc24b13ea29ea5a6805975379081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "520df93e806c4abda2d8",
+    "url": "/static/css/main.24e34c59.chunk.css"
+  },
+  {
+    "revision": "b2a0c08bcaf7b20add9a",
+    "url": "/static/js/2.3cff3dcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3cff3dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520df93e806c4abda2d8",
+    "url": "/static/js/main.893d48c8.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
