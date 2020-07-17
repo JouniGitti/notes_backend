6fbe4fdc08ca12059488d68c8bb4319bcfd4bd98
jounikoo <jouni.kullervo.kari@gmail.com>
2020-07-17 16:48:10 +0300

    Added functionalities, connect to MongoDB and Heroku

--- models/note.js
+++ models/note.js
@@ -4,6 +4,9 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
+mongoose.set('useFindAndModify', false); 
+// to prevent deprecationwarning
+
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
     console.log('connected to MongoDB')
@@ -13,8 +16,15 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
+  content: {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  date: { 
+    type: Date,
+    required: true
+  },
   important: Boolean,
 })
 
